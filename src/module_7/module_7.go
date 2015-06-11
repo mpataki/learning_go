d511e50deafda0bf4404127cab8f6274bee472fe
mpataki <matpataki@gmail.com>
2015-06-11 08:23:40 -0400

    method declarations

--- src/module_7/module_7.go
+++ src/module_7/module_7.go
@@ -2,6 +2,81 @@ package main
 
 import "fmt"
 
-func main() {
+type Salutation struct {
+  Name     string
+  Greeting string
+}
 
+type Salutations []Salutation
+
+type Printer func(string)
+
+func(salutations Salutations) Greet(do Printer, isFormal bool, times int){
+  for _, s := range salutations {
+    message1, message2 := CreateMessage(s.Name, s.Greeting)
+
+    if prefix := GetPrefix(s.Name); isFormal {
+      do(prefix + message1)
+    } else {
+      do(message2)
+    }
+  }
+}
+
+func GetPrefix(name string) (prefix string) {
+  switch name {
+  case "Bob":
+    prefix = "Mr. "
+  case "Joe", "Amy":
+    prefix = "Dr. "
+  case "Mary":
+    prefix = "Mrs. "
+  default:
+    prefix = "Dude "
+  }
+
+  return
+}
+
+func TypeSwitchTest(x interface{}) {
+  switch x.(type) {
+  case int:
+    fmt.Println("int")
+  case string:
+    fmt.Println("string")
+  case Salutation:
+    fmt.Println("salutation")
+  default:
+    fmt.Println("unknown")
+  }
+}
+
+func CreateMessage(name string, greeting string) (message1 string, message2 string) {
+  message1 = greeting + " " + name
+  message2 = "HEY! " + name
+  return
+}
+
+func Print(s string) {
+  fmt.Print(s)
+}
+
+func PrintLine(s string) {
+  fmt.Println(s)
+}
+
+func CreatePrintFunction(custom string) Printer {
+  return func(s string) {
+    fmt.Println(s + custom)
+  }
+}
+
+func main() {
+  salutations := Salutations{
+    {"Bob", "Hello"},
+    {"Joe", "Hi"},
+    {"Merry", "What is up?"},
+  }
+
+  salutations.Greet(CreatePrintFunction("?"), true, 6)
 }
--- src/module_7/module_7_notes.md
+++ src/module_7/module_7_notes.md
@@ -1,1 +1,14 @@
 ### Module 7: Methods & Interfaces
+
+Methods and interfaces in go are quite different from other languages. This is largely because go doesn't have the concept of a class. But as it turns out, go provides a considerable amount of flexibility.
+
+Let's begin with methods. This is pretty interesting because generally methods are function on classes, but in go we don't have the concept of a class. In this case, a method is a function that operates on a named type. This means that we can define a struct, and then define methods on it. Methods can also operate on pointers to named type. This allows us have that method operates on a particular instance of the type.
+
+It be noted that if we want to declare a method on a type that is provided by the language, we need to first name it something else with the `type` keyword.
+```go
+type Salutations []Salutation
+
+func(salutations Salutations)Greet(do Printer, isFormal bool, times int){
+  //...
+}
+```
