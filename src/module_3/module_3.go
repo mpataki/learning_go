8a9edd2f3631648a00d460e4026e28ba3dee7b5f
Mat Pataki <matpataki@gmail.com>
2025-11-19 11:00:47 -0500

    add a go module so we can import the greeting module

--- src/module_3/module_3.go
+++ src/module_3/module_3.go
@@ -1,9 +1,9 @@
 package main
 
-import "../greeting"
+import "learning_go/src/greeting"
 
 func main() {
 	var s = greeting.Salutation{"Joe", "Hello"}
-	greeting.Greet(s, greeting.CreatePrintFunction("!!!"), true)
+	greeting.Greet(s, greeting.CreatePrintFunction("!!!"), true, 1)
 	greeting.TypeSwitchTest(1)
 }
